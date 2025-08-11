5002d8f3cd4528f0c2aba427e8f7b08bdc6bb356
David Doyle-Owen <thinkimagine@outlook.com>
2025-08-11 19:02:42 +0100

    Add loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,16 +2,19 @@
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
 
-    CSS from here: 
+    CSS from here:
     https://stripe.com/docs/stripe-js
 */
 
+// Get keys from the JSON script tags
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
+
+// Init Stripe + Elements
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
-/* From stripe styles */
+/* From Stripe styles */
 var style = {
     base: {
         color: '#000',
@@ -23,6 +26,7 @@ var style = {
     invalid: { color: '#dc3545', iconColor: '#dc3545' }
 };
 
+// Mount card element
 var card = elements.create('card', { style: style });
 card.mount('#card-element');
 
@@ -47,10 +51,12 @@ form.addEventListener('submit', function (ev) {
     card.update({ 'disabled': true });
     $('#submit-button').attr('disabled', true);
 
+    // Show overlay while confirming payment (tutorial uses fadeToggle)
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
     stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
+        payment_method: { card: card }
     }).then(function (result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
@@ -58,6 +64,10 @@ form.addEventListener('submit', function (ev) {
                 <span class="icon" role="alert"><i class="fas fa-times"></i></span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+
+            // Restore UI on error
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false });
             $('#submit-button').attr('disabled', false);
         } else {
