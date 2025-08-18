20a44cc627e6fb97c5bf607c227e36adbf5c8cb3
David Doyle-Owen <thinkimagine@outlook.com>
2025-08-18 14:42:51 +0100

    Update allauth templates and profiles

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)lj2waiaar-+a8*g&8@9%(l-tz#6t&d6cb1l3y6^cjv*q=tv@u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
