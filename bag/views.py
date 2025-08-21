782d64a45f69f793394a32900a0e10d3dcd21153
David Doyle-Owen <thinkimagine@outlook.com>
2025-08-21 09:38:22 +0100

    Fix f-string quoting in bag/views

--- bag/views.py
+++ bag/views.py
@@ -34,7 +34,7 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] += quantity
                 messages.success(
                     request,
-                    f"Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}"
+                    f"Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}"
                 )
             else:
                 bag[item_id]['items_by_size'][size] = quantity
@@ -82,7 +82,7 @@ def adjust_bag(request, item_id):
             bag[item_id]['items_by_size'][size] = quantity
             messages.success(
                 request,
-                f"Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}"
+                f"Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}"
             )
         else:
             del bag[item_id]['items_by_size'][size]
