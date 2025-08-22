4e642a1fbd794e80944b416bba43819b93cae282
David Doyle-Owen <thinkimagine@outlook.com>
2025-08-22 15:32:56 +0100

    Fix: Update Bucket name in settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,10 +185,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ:
+if os.environ.get('USE_AWS') == 'True':
     # Bucket config
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('imn-boutiqueado')
-    AWS_S3_REGION_NAME = os.environ.get('eu-west-2')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-2')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
